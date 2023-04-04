33f3849feacd12b1113b54735eca8eb5d1705b7b
pcw <pcw@gamil.com>
2023-04-04 11:41:09 +0900

    fixReply

--- src/main/java/controller/moim/MoimReplysController.java
+++ src/main/java/controller/moim/MoimReplysController.java
@@ -1,6 +1,8 @@
 package controller.moim;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,20 +13,31 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
-import data.Moim;
+import data.User;
+
 
 @WebServlet("/moim/replys")
 public class MoimReplysController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String moimId = req.getParameter("moimId");
+		String userId = req.getParameter("userId");
+		String writer = req.getParameter("writer");
+		
+		User logonUser = (User)req.getSession().getAttribute("logonUser");
+		if(logonUser == null ) {
+			resp.sendRedirect("/user/login");
+		}
+		
 		SqlSessionFactory factory= 
 				(SqlSessionFactory)req.getServletContext().getAttribute("sqlSessionFactory");
 		SqlSession sqlSession = factory.openSession();
 		
-		String target = req.getParameter("target"); // 모임ID
-		// 모임 ID에 해당하는 정보를 읽어온 후 (mybatis 방식으로 처리)
-		Moim found = sqlSession.selectOne("replys", target);
-		
+		Map<String, Object> params = new HashMap<>();
+		params.put("moimId",moimId );
+		params.put("userId",userId );
+		params.put("writer",writer);
 		
 		sqlSession.commit();
 		sqlSession.close();
